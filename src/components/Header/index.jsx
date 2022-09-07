b4dbeaa6491b67284b7be68edcc7ea3f7fbbffab
Alana Silva <alana_brito@live.com>
2022-09-07 20:51:41 -0300

    cadastro

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,11 +1,24 @@
 import React from 'react'
 import logo from '../../assets/logo-dio.png';
 
+import { useNavigate  } from "react-router-dom";
+
 import { Button } from '../Button';
 
 import { Container, Wrapper, BuscarInputContainer, Input, Row, Menu, MenuRight, UserPicture} from './styles';
 
 const Header = ({autenticado}) => {
+
+  const navigate = useNavigate();
+
+  const handleClickSignIn = () => {
+      navigate('/login')
+  }
+
+  const handleClickRegister = () => {
+    navigate('/cadastro')
+}
+  
   return (
     <Wrapper>
       <Container>
@@ -23,12 +36,12 @@ const Header = ({autenticado}) => {
           </Row>
           <Row>
               {autenticado ? (
-                <UserPicture src="https://avatars.githubusercontent.com/u/45184516?v=4"/>
+                <UserPicture src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTS1EUJhfkJFRPWrm0_xMgLz7rxsnr81oS5iZ3ly6AEujgWMbOoIFi8OBgkARmQhjl5X_Y&usqp=CAU"/>
               ) : (
               <>
                 <MenuRight href="/">Home</MenuRight>
-                <Button title="Entrar" />
-                <Button title="Cadastrar" />
+                <Button title="Entrar" onClick={handleClickSignIn} />
+                <Button title="Cadastrar"  onClick={handleClickRegister} />
               </>)}
           </Row>
       </Container>
--- src/pages/feed/index.jsx
+++ src/pages/feed/index.jsx
@@ -26,10 +26,10 @@ const Feed = () => {
             </Column>
             <Column flex={1}>
               <TitleHighlight> # RANKING 5 TOP DA SEMANA </TitleHighlight>
-                <UserInfo nome="Pablo Henrique" image="https://avatars.githubusercontent.com/u/45184516?v=" percentual={25}/>
-                <UserInfo nome="Pablo Henrique" image="https://avatars.githubusercontent.com/u/45184516?v=" percentual={65}/>
-                <UserInfo nome="Pablo Henrique" image="https://avatars.githubusercontent.com/u/45184516?v=" percentual={45}/>
-                <UserInfo nome="Pablo Henrique" image="https://avatars.githubusercontent.com/u/45184516?v=" percentual={72}/>
+                <UserInfo nome="Aluno 1" image="https://img.freepik.com/vetores-premium/perfil-de-avatar-de-homem-no-icone-redondo_24640-14044.jpg?w=740" percentual={25}/>
+                <UserInfo nome="Aluno 2" image="https://img.freepik.com/vetores-premium/perfil-de-avatar-de-mulher-no-icone-redondo_24640-14042.jpg" percentual={65}/>
+                <UserInfo nome="Aluno 3" image="https://img.freepik.com/vetores-premium/perfil-de-avatar-de-mulher-no-icone-redondo_24640-14047.jpg" percentual={45}/>
+                <UserInfo nome="Aluno 4" image="https://img.freepik.com/vetores-premium/perfil-de-avatar-de-homem-no-icone-redondo_24640-14049.jpg" percentual={72}/>
             </Column>
         </Container>
     </>
