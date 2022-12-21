4808e7187a448afdd9596210290b95affd21a3cc
drikadev <adriguti.adriana@hotmail.com>
2022-12-21 15:09:05 -0300

    início do projeto

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,13 +1,10 @@
-import { useNavigate  } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { MdEmail, MdLock } from 'react-icons/md'
 import { Button } from '../../components/Button';
 import { Header } from '../../components/Header';
 import { Input } from '../../components/Input';
 import { api } from '../../services/api';
-
 import { useForm } from "react-hook-form";
-
-
 import { Container, Title, Column, TitleLogin, SubtitleLogin, EsqueciText, CriarText, Row, Wrapper } from './styles';
 
 const Login = () => {
