de9b21bdc24e9ce083c1847d83029e196dd424c2
Claudia Hirose <claudiahirose@gmail.com>
2023-04-29 11:08:25 -0300

    Desafio3

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import { Feed } from "./pages/feed";
 
 import { Home } from './pages/home'
 import { Login } from './pages/login'
+import { CreateAccount } from './pages/createAccount'
 import { GlobalStyle } from './styles/global';
 
 function App() {
@@ -17,6 +18,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/feed" element={<Feed />} />
+        <Route path="/createAccount" element={<CreateAccount />} />
      </Routes >
     </Router>
   );
