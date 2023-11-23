34622a80a707d1e33d3135e025c0f0fa2e892e3f
leandroFFF <leandrofrancagti@gmail.com>
2023-11-22 21:19:27 -0300

    Criado tela de cadastro

---
+++ src/pages/cadastro/styles.js
@@ -0,0 +1,84 @@
+import styled from "styled-components";
+
+export const Container = styled.main`
+    width: 100%;
+    max-width: 90%;
+    margin: 0 auto;
+    margin-top: 120px;
+
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+`
+export const Wrapper = styled.div`
+    max-width: 380px
+`
+export const Column = styled.div`
+    flex: 1;
+`
+export const Row = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: flex-start;
+    align-items: center;
+`
+export const TextContent = styled.p`
+    font-family: 'Open Sans';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 16px;
+    width: 420px;
+    margin-bottom: 20px;
+    line-height: 22px;
+
+    color: #ffffff;
+`
+export const Title = styled.h2`
+    font-family: 'Open Sans';
+    font-style: normal;
+    font-weight: 700;
+    font-size: 32px;
+    width: 320px;
+    margin-bottom: 20px;
+    line-height: 44px;
+
+    color: #ffffff;
+`
+export const TitleCadastro = styled.p`
+    font-family: 'Open Sans';
+    font-style: normal;
+    font-weight: 700;
+    font-size: 32px;
+    margin-bottom: 20px;
+    line-height: 44px;
+`
+export const SubTitleCadastro = styled.p`
+    font-family: 'Open Sans';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 18px;
+    margin-bottom: 35px;
+    line-height: 25px;
+`
+export const JaTenhoConta = styled.p`
+    font-family: 'Open Sans';
+    font-style: normal;
+    font-weight: 700;
+    font-size: 14px;
+    line-height: 19px;
+`
+export const FazerLogin = styled.p`
+    font-family: 'Open Sans';
+    font-style: normal;
+    font-weight: 700;
+    font-size: 14px;
+    line-height: 19px;
+    margin-left: 10px;
+    color: #23DD7A;
+    cursor: pointer;
+
+    &:hover {
+        opacity: 0.6;
+    }
+`
